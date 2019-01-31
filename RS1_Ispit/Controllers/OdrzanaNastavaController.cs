83415a6164463308a1413605cfe6bfe2e31e3d0f
haris-begluk <harisbegluk@gmail.com>
2019-01-31 21:54:10 +0100

    Zadatak 1 korak 1

---
+++ RS1_Ispit/Controllers/OdrzanaNastavaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_Ispit_asp.net_core.EF;
+using RS1_Ispit_asp.net_core.ViewModels;
+
+namespace RS1_Ispit_asp.net_core.Controllers
+{
+    public class OdrzanaNastavaController : Controller
+    {
+        MojContext context;
+        public OdrzanaNastavaController(MojContext mojContext)
+        {
+            context = mojContext;
+        }
+        public IActionResult Index()
+        {
+            List<SkolaNastavniciVM> model = context.Nastavnik.Select(x => new SkolaNastavniciVM
+            {
+                NastavnikID = x.Id,
+                Nastavnik = x.Ime + " " + x.Prezime,
+                Skola = context.Odjeljenje.Include(b => b.Skola).FirstOrDefault(g => g.RazrednikID == x.Id).Skola.Naziv
+
+            }).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ RS1_Ispit/ViewModels/SkolaNastavniciVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class SkolaNastavniciVM
+    {
+        public int NastavnikID { get; set; }
+        public string Nastavnik { get;  set; }
+        public string Skola { get;  set; }
+    }
+}

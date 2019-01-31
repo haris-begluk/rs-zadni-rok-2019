d2c9d65192b2f07e52f80faf0510932e8dce485f
haris-begluk <harisbegluk@gmail.com>
2019-01-31 22:10:40 +0100

    zadatak 1 korak 2

--- RS1_Ispit/Controllers/OdrzanaNastavaController.cs
+++ RS1_Ispit/Controllers/OdrzanaNastavaController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RS1_Ispit_asp.net_core.EF;
+using RS1_Ispit_asp.net_core.EntityModels;
 using RS1_Ispit_asp.net_core.ViewModels;
 
 namespace RS1_Ispit_asp.net_core.Controllers
@@ -25,6 +26,27 @@ namespace RS1_Ispit_asp.net_core.Controllers
                 Skola = context.Odjeljenje.Include(b => b.Skola).FirstOrDefault(g => g.RazrednikID == x.Id).Skola.Naziv
 
             }).ToList();
+            return View(model);
+        }
+        public IActionResult Odaberi(int id)
+        {
+
+            MaturskiIspitVM model = new MaturskiIspitVM
+            {
+                NastavnikId=id,
+                Rows=context.MaturskiIspit.Include(h=>h.Skola).Include(j=>j.Predmet).Where(x=>x.NastavnikId==id).Select(b=>new MaturskiIspitVM.Row
+                {
+                    MaturskiIspitId=b.Id,
+                    Datum=b.Datum.ToString("dd.MM.yyyy"),
+                    Skola=b.Skola.Naziv,
+                    Predmet=b.Predmet.Naziv,
+                    NisuPristupili=context.MaturskiIspitStavka.Include(n=>n.Ucenik)
+                    .Where(g=>g.MaturskiIspitId==b.Id && g.PristupioIspitu==false).Select(f=>f.Ucenik.ImePrezime).ToList()
+
+                }).ToList()
+                
+            };
+
             return View(model);
         }
     }
---
+++ RS1_Ispit/ViewModels/MaturskiIspitVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class MaturskiIspitVM
+    {
+        public int NastavnikId { get; set; }
+        public List<Row> Rows { get; set; }
+        public class Row
+        {
+            public int MaturskiIspitId { get;  set; }
+            public string Datum { get;  set; }
+            public string Skola { get;  set; }
+            public string Predmet { get;  set; }
+            public List<string> NisuPristupili { get;  set; }
+        }
+    }
+}
